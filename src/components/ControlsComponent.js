15b9d6ba01ed1ec3fc55db3815da4f78ca81c531
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-06 02:13:07 +0100

    recipe search setup completed

--- src/components/ControlsComponent.js
+++ src/components/ControlsComponent.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchComponent from "./SearchComponent";
 
 const ControlsComponent = () => {
+  const [recipeSearchFilter, setRecipeSearchFilter] = useState({
+    name: "",
+    type: "",
+    diet: "",
+  });
+
+  const getRecipies = () => {
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOONACULAR_PRIVATE_KEY}&query=pasta&diet=gluten free&type=main course`;
+  };
+
   return (
     <>
       <SearchComponent />
